e1508bed77eaf9677d36bcf155b484f5e876f26f
Illya Maznitskiy <comercaleuros@gmail.com>
2024-09-11 15:12:28 +0300

    Create views.py for the english_school app

--- backend/english_school/views.py
+++ backend/english_school/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from backend.english_school.models import (
+    Teacher,
+    Course,
+    Review,
+    ContactMessage,
+)
+from backend.english_school.serializers import (
+    TeacherSerializer,
+    CourseSerializer,
+    ReviewSerializer,
+    ContactMessageSerializer,
+)
+
+
+class TeacherViewSet(viewsets.ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+
+
+class ContactMessageViewSet(viewsets.ModelViewSet):
+    queryset = ContactMessage.objects.all()
+    serializer_class = ContactMessageSerializer
