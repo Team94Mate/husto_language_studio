e8a8b410b725aa50b27a85cd7d10b908dedda565
Illya Maznitskiy <comercaleuros@gmail.com>
2024-09-18 16:50:58 +0300

    Update views.py with correct permissions

--- backend/english_school/views.py
+++ backend/english_school/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import BasePermission, IsAdminUser
 
 from english_school.models import (
     Teacher,
@@ -14,21 +15,46 @@ from backend.english_school.serializers import (
 )
 
 
+class IsAdminOrReadOnly(BasePermission):
+    """
+    Custom permission to only allow admins to edit or delete objects.
+    """
+    def has_permission(self, request, view):
+        if request.method in ['GET']:
+            return True
+        return request.user and request.user.is_staff
+
+
+class IsAdminOrCreateAndReadOnly(BasePermission):
+    """
+    Custom permission to allow all users to create and view,
+    but only admins can update or delete.
+    """
+    def has_permission(self, request, view):
+        if request.method in ['GET', 'POST']:
+            return True
+        return request.user and request.user.is_staff
+
+
 class TeacherViewSet(viewsets.ModelViewSet):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class CourseViewSet(viewsets.ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    permission_classes = [IsAdminOrReadOnly]
 
 
 class ContactMessageViewSet(viewsets.ModelViewSet):
     queryset = ContactMessage.objects.all()
     serializer_class = ContactMessageSerializer
+    permission_classes = [IsAdminOrCreateAndReadOnly]
