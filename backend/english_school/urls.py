ae657c6350fff156417b65db28fdfb09e803afbe
Illya Maznitskiy <comercaleuros@gmail.com>
2024-09-18 16:49:09 +0300

    Add basename for English school urls

--- backend/english_school/urls.py
+++ backend/english_school/urls.py
@@ -10,10 +10,14 @@ from english_school.views import (
 
 
 router = DefaultRouter()
-router.register(r"teachers", TeacherViewSet)
-router.register(r"courses", CourseViewSet)
-router.register(r"reviews", ReviewViewSet)
-router.register(r"contact-messages", ContactMessageViewSet)
+router.register(r"teachers", TeacherViewSet, basename="teachers")
+router.register(r"courses", CourseViewSet, basename="courses")
+router.register(r"reviews", ReviewViewSet, basename="reviews")
+router.register(
+    r"contact-messages",
+    ContactMessageViewSet,
+    basename="contact-messages"
+)
 
 urlpatterns = [
     path("", include(router.urls)),
