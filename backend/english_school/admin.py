fb65de0d0851ec4c3aedc3e16c9f9eedccab7513
Illya Maznitskiy <comercaleuros@gmail.com>
2024-09-23 15:43:49 +0300

    Register models in admin.py for management in admin interface

--- backend/english_school/admin.py
+++ backend/english_school/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Teacher, Course, Review, ContactMessage
+
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "specialization",
+        "experience_years",
+        "teacher_level",
+    )
+    search_fields = ("name", "specialization")
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    list_display = ("title", "course_type", "duration", "price")
+    search_fields = ("title", "course_type")
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("name", "age")
+    search_fields = ("name",)
+
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ("username", "submitted_at")
+    search_fields = ("username",)
