bfd46cd479d4aed5b6b419611fb7186eaa4213fa
Illya Maznitskiy <comercaleuros@gmail.com>
2024-10-09 16:13:39 +0300

    Add documentation URLs in school_service/urls.py

--- backend/school_service/urls.py
+++ backend/school_service/urls.py
@@ -19,11 +19,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("english_school.urls")),
+    path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
 ] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )  # Remove the line
