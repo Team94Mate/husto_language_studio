1782a8ba2fda5628b32231637125abc7d4ba4ea2
Illya Maznitskiy <comercaleuros@gmail.com>
2024-10-10 18:42:36 +0300

    Update ContactMessage view tests to new permission

--- backend/english_school/tests/test_views.py
+++ backend/english_school/tests/test_views.py
@@ -302,13 +302,11 @@ class PublicContactMessageViewSetTests(APITestCase):
 
     def test_get_contactmessage_list(self):
         response = self.client.get(self.list_url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
     def test_get_contactmessage_detail(self):
         response = self.client.get(self.detail_url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(response.data["username"], "user1")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
 
 class PrivateContactMessageViewSetTests(APITestCase):
