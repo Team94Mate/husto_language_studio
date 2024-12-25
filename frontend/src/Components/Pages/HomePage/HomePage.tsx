c00829544db29be849e6fef0a10f88066373c315
Vitaliy Zviriuk <zvirvitalik1991@gmail.com>
2024-12-25 15:51:08 +0100

    fixed homePage backround

--- frontend/src/Components/Pages/HomePage/HomePage.tsx
+++ frontend/src/Components/Pages/HomePage/HomePage.tsx
@@ -45,95 +45,95 @@ export const HomePage = () => {
       <button className="homePage__mob-button" onClick={handleClick}>
         <img className="homePage__mob-image" src="images/HeadericonMenu.svg" />
       </button>
-      <div className="homePage__wrapper">
-        <div className="homePage__container">
-          <h1 className="homePage__title animation left">
-            Твій успіх - наша мета
-          </h1>
-          <p className="homePage__from animation right">From</p>
+      {/* <div className="homePage__wrapper"> */}
+      <div className="homePage__container">
+        <h1 className="homePage__title animation left">
+          Твій успіх - наша мета
+        </h1>
+        <p className="homePage__from animation right">From</p>
+        <img
+          src="images/DodBeginner.png"
+          alt="Small Dog"
+          className="homePage__dog1 animation right"
+        />
+
+        <p className="homePage__beginner animation right">Beginner</p>
+        <div className="homePage__box-to animation right">
           <img
-            src="images/DodBeginner.png"
-            alt="Small Dog"
-            className="homePage__dog1 animation right"
+            src="images/Header-small-arrow.svg"
+            alt="Arrow"
+            className="homePage__to"
           />
-
-          <p className="homePage__beginner animation right">Beginner</p>
-          <div className="homePage__box-to animation right">
-            <img
-              src="images/Header-small-arrow.svg"
-              alt="Arrow"
-              className="homePage__to"
-            />
-          </div>
-
-          <p className="homePage__advanced animation right">Advanced</p>
-          <h1 className="homePage__husto animation top">HUSTO</h1>
-          <div className="homePage__hash-box animation left">
-            <div className="homePage__hash" />
-          </div>
-
-          <img
-            src="images/big-dog.png"
-            alt="advenced dog"
-            className="homePage__dog2 animation right"
-          />
-          <h1 className="homePage__h1 animation right">
-            Мовна школа яка змінить тебе!
-          </h1>
-          <p className="homePage__p animation left">
-            Індивідуальні, парні та групові онлайн курси <br />
-            на інтерактивній платформі <br />
-            з досвідченими викладачами <br />
-          </p>
-
-          <a
-            className="homePage__button homePage__button--1 animation right"
-            onClick={() => {
-              setIsClicked(prev => [...prev, 1]);
-            }}
-            target="_blank"
-            href={
-              `${'https://www.canva.com/design/DAFtevtJOXM/'}` +
-              `${'6IYP4BRYWjpF5rxkheWVew/edit'}`
-            }
-            rel="noreferrer"
-          >
-            <h3 className="homePage__button-text1"> Безкоштовний марафон</h3>
-            <span className="homePage__button2 homePage__button2--phone">
-              <img
-                className={classNames('homePage__button-img', {
-                  'homePage__button-img--clicked1': isClicked.includes(1),
-                })}
-                alt="buttin icon"
-                src="images/Vector(6).svg"
-              />
-            </span>
-          </a>
-
-          <a
-            className="homePage__button homePage__button--2 animation right"
-            onClick={() => setIsClicked(prev => [...prev, 2])}
-            target="_blank"
-            href={
-              `${'https://docs.google.com/forms/d/e/1FAIpQLSc'}` +
-              `${'__p8783rA2XzmtrQQsdfGPB2I9CTCfFF_6gzSBdSzDDInVg/viewform'}`
-            }
-            rel="noreferrer"
-          >
-            <h3 className="homePage__button-text2">Безкоштовне тестування</h3>
-
-            <span className="homePage__button2 homePage__button2--phone">
-              <img
-                className={classNames('homePage__button-img', {
-                  'homePage__button-img--clicked2': isClicked.includes(2),
-                })}
-                src="images/Vector(6).svg"
-                alt="buttin icon"
-              />
-            </span>
-          </a>
         </div>
+
+        <p className="homePage__advanced animation right">Advanced</p>
+        <h1 className="homePage__husto animation top">HUSTO</h1>
+        <div className="homePage__hash-box animation left">
+          <div className="homePage__hash" />
+        </div>
+
+        <img
+          src="images/big-dog.png"
+          alt="advenced dog"
+          className="homePage__dog2 animation right"
+        />
+        <h1 className="homePage__h1 animation right">
+          Мовна школа яка змінить тебе!
+        </h1>
+        <p className="homePage__p animation left">
+          Індивідуальні, парні та групові онлайн курси <br />
+          на інтерактивній платформі <br />
+          з досвідченими викладачами <br />
+        </p>
+
+        <a
+          className="homePage__button homePage__button--1 animation right"
+          onClick={() => {
+            setIsClicked(prev => [...prev, 1]);
+          }}
+          target="_blank"
+          href={
+            `${'https://www.canva.com/design/DAFtevtJOXM/'}` +
+            `${'6IYP4BRYWjpF5rxkheWVew/edit'}`
+          }
+          rel="noreferrer"
+        >
+          <h3 className="homePage__button-text1"> Безкоштовний марафон</h3>
+          <span className="homePage__button2 homePage__button2--phone">
+            <img
+              className={classNames('homePage__button-img', {
+                'homePage__button-img--clicked1': isClicked.includes(1),
+              })}
+              alt="buttin icon"
+              src="images/Vector(6).svg"
+            />
+          </span>
+        </a>
+
+        <a
+          className="homePage__button homePage__button--2 animation right"
+          onClick={() => setIsClicked(prev => [...prev, 2])}
+          target="_blank"
+          href={
+            `${'https://docs.google.com/forms/d/e/1FAIpQLSc'}` +
+            `${'__p8783rA2XzmtrQQsdfGPB2I9CTCfFF_6gzSBdSzDDInVg/viewform'}`
+          }
+          rel="noreferrer"
+        >
+          <h3 className="homePage__button-text2">Безкоштовне тестування</h3>
+
+          <span className="homePage__button2 homePage__button2--phone">
+            <img
+              className={classNames('homePage__button-img', {
+                'homePage__button-img--clicked2': isClicked.includes(2),
+              })}
+              src="images/Vector(6).svg"
+              alt="buttin icon"
+            />
+          </span>
+        </a>
       </div>
+      {/* </div> */}
     </div>
   );
 };
